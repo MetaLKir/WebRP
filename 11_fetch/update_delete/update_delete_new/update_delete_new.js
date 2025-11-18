6984dd5671af98ffb63162935774d5d4a6a2e368
MetaL Kir <masterzatk@gmail.com>
2025-11-18 13:36:01 +0200

    Lesson 12 classwork update-delete old

---
+++ 11_fetch/update_delete/update_delete_new/update_delete_new.js
@@ -0,0 +1,71 @@
+const baseUrl = "https://reqres.in/api";
+const API_KEY = 'reqres-free-v1';
+// "email" : "eve.holt@reqres.in",
+// "password" : "pistol"
+
+const result = document.querySelector('#result');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
+const userIdInput = document.querySelector('#userId');
+const updateBtn = document.querySelector('#update');
+const deleteBtn = document.querySelector('#delete');
+
+//===== Update =====
+updateBtn.onclick = async () => {
+    const id = userIdInput.value.trim();
+    const name = nameInput.value.trim();
+    const job = jobInput.value.trim();
+    if (!id) {
+        result.textContent = "Error: user id is required";
+        return;
+    }
+    const body = {name, job}
+    result.textContent = "Sending update request...";
+    try {
+        const response = await fetch(`${baseUrl}/users/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-api-key': API_KEY
+            },
+            body: JSON.stringify(body),
+        });
+        const data = await response.json();
+        if (!response.ok) {
+            const msg = data.error || 'HTTP error: ' + response.status;
+            throw new Error(msg);
+        }
+        console.log("Updated user: ", data);
+        result.textContent = JSON.stringify(data);
+    } catch (e) {
+        console.error("Error: ", e.message);
+        result.textContent = "Error: " + e.message;
+    }
+}
+
+//===== Delete =====
+deleteBtn.onclick = async () => {
+    const id = userIdInput.value.trim();
+    if (!id) {
+        result.textContent = "Error: user id is required";
+        return;
+    }
+    result.textContent = "Sending update request...";
+    try {
+        const response = await fetch(`${baseUrl}/users/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'x-api-key': API_KEY
+            }
+        })
+        if(!response.ok){
+            throw new Error("HTTP error: " + response.status);
+        }
+        console.log("Deleted user: ", id, ", status: ", response.status);
+        result.textContent = `User ${id} has been deleted, status: ${response.status}`;
+    } catch (e) {
+        console.error("Error: ", e.message);
+        result.textContent = "Error: " + e.message;``
+    }
+}
