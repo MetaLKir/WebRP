6d7052affe7b8286e8a2b8610b36790c8fb44002
MetaL Kir <masterzatk@gmail.com>
2025-11-21 15:53:31 +0200

    Lesson 13 homework NASA images

---
+++ 13_homework_NASA/nasa/nasa.js
@@ -0,0 +1,132 @@
+const BASE_URL = "https://api.nasa.gov/planetary/apod"
+
+// ===== input fields =====
+const dateInput = document.querySelector("#dateInput");
+const dateFromInput = document.querySelector("#dateFromInput");
+const dateToInput = document.querySelector("#dateToInput");
+const amountInput = document.querySelector("#amountInput");
+const apiKeyInput = document.querySelector("#apiKeyInput");
+// ===== buttons =====
+const imgByDateBtn = document.querySelector("#imgByDateBtn");
+const imgByDateRangeBtn = document.querySelector("#imgByDateRangeBtn");
+const imgByAmountBtn = document.querySelector("#imgByAmountBtn");
+// ===== result =====
+const resultContainer = document.querySelector('#resultContainer');
+
+// ===== bind buttons =====
+imgByDateBtn.onclick = () => {
+    if (dateInput.value === "") {
+        onScreenWarning();
+    } else {
+        fetchPicture({date: dateInput.value});
+    }
+}
+imgByDateRangeBtn.onclick = () => {
+    if (dateFromInput.value === "") {
+        onScreenWarning();
+    } else {
+        fetchPicture({
+            start_date: dateFromInput.value,
+            end_date: dateToInput.value,
+        });
+    }
+}
+imgByAmountBtn.onclick = () => {
+    if(amountInput.value === "") {
+        onScreenWarning();
+    } else if (Number(amountInput.value) <= 0){
+        onScreenWarning("Incorrect amount, enter at least 1");
+    }
+    else {
+        fetchPicture({count: amountInput.value});
+    }
+}
+
+// ===== logic =====
+function displayResult(data, container) {
+    const wrap = document.createElement('div');
+    wrap.style.marginBottom = '20px';
+    const h3 = document.createElement('h3');
+    h3.textContent = data.title || "no title";
+    wrap.appendChild(h3);
+    if (data.media_type === 'image') {
+        const img = document.createElement('img');
+        img.src = data.hdurl || data.url;
+        img.alt = data.title || "APOD image";
+
+        img.style.maxWidth = '800px';
+        img.style.maxHeight = '600px';
+        img.style.width = '100%';
+        img.style.height = 'auto';
+
+        wrap.appendChild(img);
+    } else if (data.media_type === "video") {
+        const iframe = document.createElement('iframe');
+        iframe.src = data.url;
+        iframe.width = "560";
+        iframe.height = "315";
+        iframe.allowFullScreen = true;
+        iframe.loading = "lazy";
+
+        wrap.appendChild(iframe);
+
+        if (data.thumbnail_url) {
+            const thumb = document.createElement('img');
+            thumb.src = data.thumbnail_url;
+            thumb.alt = "Video thumbnail";
+            wrap.appendChild(thumb);
+        }
+    } else {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = `unsupported media type ${data.media_type}`;
+        wrap.appendChild(paragraph);
+    }
+
+    const meta = document.createElement('p')
+    meta.textContent = data.date || "";
+    wrap.appendChild(meta);
+
+    const desc = document.createElement('p');
+    desc.textContent = data.explanation || "no explanation";
+    wrap.appendChild(desc);
+
+    container.appendChild(wrap);
+}
+
+async function fetchPicture(params = {}) {
+    resultContainer.innerHTML = '';
+    const baseParams = {
+        api_key: apiKeyInput.value,
+        thumbs: "True"
+    };
+    const qp = new URLSearchParams({...baseParams, ...params});
+
+    const isSingleDate = !!params.date && !params.start_date && !params.end_date && !params.count;
+
+    const doFetch = async (searchParams) => {
+        const response = await fetch(`${BASE_URL}?${searchParams}`);
+        if (!response.ok) {
+            throw response;
+        }
+        return response.json();
+    }
+    try {
+        const data = await doFetch(qp);
+        if(isSingleDate) {
+            displayResult(data, resultContainer);
+        } else {
+            data.forEach(item => {displayResult(item, resultContainer)});
+        }
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+        onScreenWarning(`Error: ${e.status}`);
+    }
+}
+
+function onScreenWarning(message = "Please, fill the matching field to get an image!"){
+    resultContainer.innerHTML = '';
+    const warning = document.createElement('h3');
+    warning.textContent = message;
+    warning.classList.add('text-warning');
+    resultContainer.appendChild(warning);
+}
